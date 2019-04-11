0bf1a93d1e32d1ccbb2c290c7705980fb11ff511
Kelvin Santos <kelvin.santos@ufca.edu.br>
2019-04-11 17:04:35 -0300

    Terminado backend omnistack 6

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,26 @@
+const Box = require("../models/Box");
+
+class BoxController {
+  async store(request, response) {
+    const box = await Box.create({
+      title: request.body.title
+    });
+
+    return response.json(box);
+  }
+
+  async show(request, response) {
+    const box = await Box.findById(request.params.id).populate({
+      path: "files",
+      options: {
+        sort: {
+          createdAt: -1
+        }
+      }
+    });
+
+    return response.json(box);
+  }
+}
+
+module.exports = new BoxController();
---
+++ src/controllers/FileController.js
@@ -0,0 +1,23 @@
+const File = require("../models/File");
+const Box = require("../models/Box");
+
+class FileController {
+  async store(request, response) {
+    const box = await Box.findById(request.params.id);
+
+    const file = await File.create({
+      title: request.file.originalname,
+      path: request.file.key
+    });
+
+    box.files.push(file);
+
+    await box.save();
+
+    request.io.sockets.in(box._id).emit('file', file);
+
+    return response.json(file);
+  }
+}
+
+module.exports = new FileController();
